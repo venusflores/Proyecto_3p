61bf3fa07a5cc68200dc747ecc3ab6770d09f5f6
ITT <jcromerohdz@gmail.com>
2024-05-29 13:23:11 -0700

    MealItem component

---
+++ components/MealItem.js
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native"
+
+function MealItem({ title }){
+    return(
+        <View>
+            <Text>
+                {{ title }}
+            </Text>
+        </View>
+    )
+
+}
+
+export default MealItem
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,33 @@
-import { View, Text, StyleSheet } from "react-native";
-import { MEALS } from "../data/dummy_data";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+//import { useRoute } from '@react-navigation/native'
 
-function MealsOverviewScreen(){
+import { MEALS } from "../data/dummy_data";
+import MealItem from "../components/MealItem";
+
+function MealsOverviewScreen({ route }){
+    const catId = route.params.categoryId
+
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >= 0
+    })
+
+    function renderMealItem() {
+        return(
+            <MealItem 
+                title={itemData.item.title}
+            />
+        )
+
+    }
+
     return(
         <View style={styles.container}>
-            <Text>Meals Overview</Text>
+            <Text>Meals Overview - {{ catId }}</Text>
+            <FlatList 
+                data={displayedMeals}
+                keyExtractor={(item) => item.id}
+                renderItem={renderMealItem}
+            />
         </View>
     )
 }
